eabf7a68ee7eadcabb45fc1da7e3bf5544564a9f
danila nikitin <danila.n2015@yandex.ru>
2023-11-02 18:47:23 +0300

    initial with database and kafka settings.gradle and application.yml

---
+++ src/main/java/com/jfund/currencyvaluesservice/CurrencyValuesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jfund.currencyvaluesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyValuesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyValuesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfund/currencyvaluesservice/CurrencyValuesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfund.currencyvaluesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyValuesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
